571126a859492805d5bc6228ced276160ffb7b7f
hitesh-bansal <hiteshbansal224@gmail.com>
2019-07-20 12:21:56 +0530

    Updating SQL project

--- 1520094343_sql_project.sql
+++ 1520094343_sql_project.sql
@@ -35,7 +35,7 @@ SELECT COUNT( name ) FROM Facilities WHERE membercost = 0.0
 where the fee is less than 20% of the facility's monthly maintenance cost?
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
-SELECT facid, name, membercost, monthlymaintenance FROM Facilities where membercost < monthlymaintenance * 0.20 
+SELECT facid, name, membercost, monthlymaintenance FROM Facilities where membercost != 0.0 and membercost < monthlymaintenance * 0.20 
 
 /* Q4: How can you retrieve the details of facilities with ID 1 and 5?
 Write the query without using the OR operator. */
